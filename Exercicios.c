b8878002caecc32a6b3a0cc8f5801a83b7ad5383
llorrainean <lorrainean31@gmail.com>
2025-04-14 22:46:46 +0000

    1

--- Exercicios.c
+++ Exercicios.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(){
+
+    int a = 10; 
+
+    if ( !a ) { //(!(a > 0)
+        printf(" a é zero. \n");
+        // printf(" a não é um número positivo\n");
+
+    }else {
+
+        printf("A Variavel é diferente de zero.");
+    }
+} 
